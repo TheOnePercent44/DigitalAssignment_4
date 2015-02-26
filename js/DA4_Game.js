4acb94406969e56ef422d5325071000f43ec4eba
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-26 01:23:13 -0500

    remove?

--- js/DA4_Game.js
+++ js/DA4_Game.js
@@ -276,17 +276,14 @@ function Horde(game, playersprite)
 	this.growTall = function(zombieFriend)//, layer)//accepts two for collide
 	{
 		console.log("In growTall");
-		if(zombieFriend.body.onWall())
-		{
+		//if(zombieFriend.body.onWall())
+		//{
 			console.log("I am onWall()");
-			if(zombieFriend.body.velocity.x > 0)
-				zombieFriend.body.velocity.x = 1;
-			else
-				zombieFriend.body.velocity.x = -1;
+			zombieFriend.body.velocity.x = 0;
 			//zombieFriend.body.velocity.y = this.game.gravity.y+(32*this.zombies.getIndex(zombieFriend));
 			zombieFriend.body.y = zombieFriend.body.y-(32*this.zombies.getIndex(zombieFriend));
 			this.stacked = true;
-		}
+		//}
 	}
 };
 
