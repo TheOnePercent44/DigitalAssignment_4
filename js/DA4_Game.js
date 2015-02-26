652868c93dab905c0c84aba971b4bbd0271b5a5e
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-25 23:02:51 -0500

    Climb?

--- js/DA4_Game.js
+++ js/DA4_Game.js
@@ -85,9 +85,20 @@ OrganTrail.Game.prototype = {
 			if(player.inAir === false)
 				player.idle();
 		}
-		if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+		//if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+		if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
 		{
 			//player.jump();
+			if(friends.climbing === false)
+			{
+				friends.climbing = true;
+				friends.ladder();
+			}
+			else
+			{
+				horde.zombies.forEachAlive(horde.zombies.setProperty, 'enableGravity', false, 0, true);
+				friends.climbing = false;
+			}
 		}
     },
 
@@ -152,12 +163,17 @@ function Horde(game, playersprite)
 	
 	this.update = function()
 	{
-		this.zombies.forEachAlive(this.chase, this);
+		if(friends.climbing === false)
+			this.zombies.forEachAlive(this.chase, this);
+		else
+		{
+			//uhhh?
+		}
 	}
 	
 	this.chase = function(zombieFriend)
 	{
-		var distance = this.game.math.distance(zombieFriend.x, zombieFriend.y, this.target.x, this.target.y);
+		/*var distance = this.game.math.distance(zombieFriend.x, zombieFriend.y, this.target.x, this.target.y);
 
 		// If the distance > MIN_DISTANCE then move
 		if (distance > this.MIN_DISTANCE && zombieFriend.body.velocity.x < this.MAX_SPEED)// && this.distance !< MAX_DISTANCE)
@@ -174,6 +190,32 @@ function Horde(game, playersprite)
 		{
 			zombieFriend.body.velocity.setTo(0, 0);
 			//zombieFriend.body.acceleration.x = 0;
+		}*/
+		this.game.physics.arcade.moveToObject(zombieFriend, this.target);//???
+	}
+	
+	this.ladder = function()
+	{
+		this.zombies.forEachAlive(this.zombies.setProperty, 'enableGravity', false, 0, true);
+		this.zombies.forEachAlive(this.findWall, this);
+	}
+	
+	this.findWall = function(zombieFriend)
+	{
+		if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))//move right and stack
+		{
+			zombieFriend.velocity.x = this.MAX_SPEED;
+			this.game.physics.arcade.collide(zombieFriend, layer, this.growTall, null, this);
+		}
+	}
+	
+	this.growTall = function(zombieFriend)
+	{
+		if(zombieFriend.body.touching.right)
+		{
+			zombieFriend.body.velocity.x = 0;
+			//zombieFriend.body.velocity.y = this.game.gravity.y+(32*this.zombies.getIndex(zombieFriend));
+			zombeFriend.body.y = zombieFriend.body.y+(32*this.zombies.getIndex(zombieFriend));
 		}
 	}
 }
