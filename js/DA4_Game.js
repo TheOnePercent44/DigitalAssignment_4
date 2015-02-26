d5a0b2efd54e0d1cb39293cf6d8242c37ca15a48
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-25 19:25:16 -0500

    sadsd

--- js/DA4_Game.js
+++ js/DA4_Game.js
@@ -64,6 +64,7 @@ OrganTrail.Game.prototype = {
 
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
 		this.game.physics.arcade.collide(player.sprite, layer, player.hitLand, null, player);
+		this.game.physics.arcade.collide(humans, layer);
 		
 		if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
 		{
